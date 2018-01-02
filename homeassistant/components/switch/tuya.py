2ff615f2aa5011953eeb43cc905d8989cf4d5d19
Nick Venenga <nijave@gmail.com>
2018-01-01 21:33:06 -0500

    Added experimental Tuya component

---
+++ homeassistant/components/switch/tuya.py
@@ -0,0 +1,97 @@
+"""
+Simple platform to control **SOME** Tuya switch devices.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/switch.tuya/
+"""
+import voluptuous as vol
+from homeassistant.components.switch import SwitchDevice, PLATFORM_SCHEMA
+from homeassistant.const import (CONF_NAME, CONF_HOST, CONF_ID)
+import homeassistant.helpers.config_validation as cv
+import logging
+
+REQUIREMENTS = ['pytuya==1.0']
+
+CONF_DEVICE_ID = 'device_id'
+CONF_LOCAL_KEY = 'local_key'
+
+DEFAULT_ID = 1
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_NAME): cv.string,
+    vol.Required(CONF_HOST): cv.string,
+    vol.Required(CONF_DEVICE_ID): cv.string,
+    vol.Required(CONF_LOCAL_KEY): cv.string,
+    vol.Optional(CONF_ID, default=DEFAULT_ID): cv.string,
+})
+
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Set up of the Tuya switch."""
+    import pytuya
+
+    add_devices([TuyaDevice(
+        pytuya.OutletDevice(
+            config.get(CONF_DEVICE_ID),
+            config.get(CONF_HOST),
+            config.get(CONF_LOCAL_KEY),
+        ),
+        config.get(CONF_NAME),
+        config.get(CONF_ID)
+    )])
+
+
+class TuyaDevice(SwitchDevice):
+    """Representation of a Tuya switch."""
+
+    def __init__(self, device, name, switchid):
+        """Initialize the Tuya switch."""
+        self._device = device
+        self._name = name
+		# TODO make sure state is reliably set (see 'update' method below)
+        self._state = False #device.status()['dps']['1']
+        self._switchid = switchid
+
+    @property
+    def name(self):
+        """Get name of Tuya switch."""
+        return self._name
+
+    @property
+    def is_on(self):
+        """Check if Tuya switch is on."""
+        return self._state
+
+    def turn_on(self, **kwargs):
+        """Turn Tuya switch on."""
+        self._device.set_status(True, self._switchid)
+
+    def turn_off(self, **kwargs):
+        """Turn Tuya switch off."""
+        self._device.set_status(False, self._switchid)
+    
+    def update(self):
+        retry_limit = 3
+		'''
+		Tuya device tends to send back a RST,ACK on first try
+		which kills the TCP connection. It seems sporadic since
+		occassionally the status update works fine on the first
+		try. Theory is the device has an issue with HA constantly
+		querying it or the connection not being closed properly.
+		
+		Regardless, it seems to work consistently with this workaround
+		in place.
+		'''
+        for i in range(retry_limit):
+            try:
+                status = self._device.status()
+                self._state = status['dps']['1']
+                return
+            except:
+                if i+1 == retry_limit:
+                    raise ConnectionRefusedError("Failed communicating with outlet after 3 tries")
+                else:
+                    logging.getLogger(__name__).warn("Try: {}/{} - failed to update information for {}".format(i+1, retry_limit, self._device))
+                    continue
+        
+        
