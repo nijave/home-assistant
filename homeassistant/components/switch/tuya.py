0c4f847c3ac0ec562a251517b7e55c201ba1b0b3
Nick Venenga <nijave@gmail.com>
2018-01-02 14:25:43 -0500

    Add device state attributes (template copied from tplink component) and fix whitespace

--- homeassistant/components/switch/tuya.py
+++ homeassistant/components/switch/tuya.py
@@ -17,6 +17,10 @@ CONF_LOCAL_KEY = 'local_key'
 
 DEFAULT_ID = 1
 
+ATTR_CURRENT = 'current'
+ATTR_CURRENT_CONSUMPTION = 'current_consumption'
+ATTR_VOLTAGE = 'voltage'
+
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_NAME): cv.string,
     vol.Required(CONF_HOST): cv.string,
@@ -48,7 +52,7 @@ class TuyaDevice(SwitchDevice):
         """Initialize the Tuya switch."""
         self._device = device
         self._name = name
-		# TODO make sure state is reliably set (see 'update' method below)
+        # TODO make sure state is reliably set (see 'update' method below)
         self._state = False #device.status()['dps']['1']
         self._switchid = switchid
 
@@ -62,6 +66,18 @@ class TuyaDevice(SwitchDevice):
         """Check if Tuya switch is on."""
         return self._state
 
+    @property
+    def device_state_attributes(self):
+        try:
+            attrs = {}
+            attrs[ATTR_CURRENT] = "{} mA".format(self._status['dps']['4'])
+            attrs[ATTR_CURRENT_CONSUMPTION] = "{} W".format(self._status['dps']['5']/10)
+            attrs[ATTR_VOLTAGE] = "{} V".format(self._status['dps']['6']/10)
+        except: # No status info
+            logging.getLogger(__name__).exception("Problem getting device state attributes")
+            attrs = None
+        return attrs
+
     def turn_on(self, **kwargs):
         """Turn Tuya switch on."""
         self._device.set_status(True, self._switchid)
@@ -72,20 +88,20 @@ class TuyaDevice(SwitchDevice):
     
     def update(self):
         retry_limit = 3
-		'''
-		Tuya device tends to send back a RST,ACK on first try
-		which kills the TCP connection. It seems sporadic since
-		occassionally the status update works fine on the first
-		try. Theory is the device has an issue with HA constantly
-		querying it or the connection not being closed properly.
-		
-		Regardless, it seems to work consistently with this workaround
-		in place.
-		'''
+        '''
+        Tuya device tends to send back a RST,ACK on first try
+        which kills the TCP connection. It seems sporadic since
+        occassionally the status update works fine on the first
+        try. Theory is the device has an issue with HA constantly
+        querying it or the connection not being closed properly.
+        
+        Regardless, it seems to work consistently with this workaround
+        in place.
+        '''
         for i in range(retry_limit):
             try:
-                status = self._device.status()
-                self._state = status['dps']['1']
+                self._status = self._device.status()
+                self._state = self._status['dps']['1']
                 return
             except:
                 if i+1 == retry_limit:
@@ -93,5 +109,4 @@ class TuyaDevice(SwitchDevice):
                 else:
                     logging.getLogger(__name__).warn("Try: {}/{} - failed to update information for {}".format(i+1, retry_limit, self._device))
                     continue
-        
-        
+
